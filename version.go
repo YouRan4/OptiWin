b54eb0ff5c79ea7f5a48109520fcfc1b618bf4c9
YouRan4 <1670044232@qq.com>
2026-07-21 19:19:08 +0800

    v1.3
    - 我优化了此版本的使用体验，然后选购了雨衣雨鞋！
    - 但是我没抢到BW的票 MD

--- version.go
+++ version.go
@@ -5,9 +5,9 @@ import (
 	"strings"
 )
 
-const CurrentVersion = "v1.2.2"
+const CurrentVersion = "v1.3"
 
-func parseVersion(v string) (major, minor int) {
+func parseVersion(v string) (major, minor, patch int) {
 	v = strings.TrimPrefix(v, "v")
 	parts := strings.SplitN(v, ".", 3)
 	if len(parts) >= 1 {
@@ -16,12 +16,19 @@ func parseVersion(v string) (major, minor int) {
 	if len(parts) >= 2 {
 		minor, _ = strconv.Atoi(parts[1])
 	}
+	if len(parts) >= 3 {
+		patchStr := parts[2]
+		if idx := strings.Index(patchStr, "-"); idx >= 0 {
+			patchStr = patchStr[:idx]
+		}
+		patch, _ = strconv.Atoi(patchStr)
+	}
 	return
 }
 
 func compareVersion(current, remote string) int {
-	cm, cn := parseVersion(current)
-	rm, rn := parseVersion(remote)
+	cm, cn, cp := parseVersion(current)
+	rm, rn, rp := parseVersion(remote)
 	if cm != rm {
 		if cm < rm {
 			return -1
@@ -30,6 +37,12 @@ func compareVersion(current, remote string) int {
 	}
 	if cn != rn {
 		if cn < rn {
+			return -1
+		}
+		return 1
+	}
+	if cp != rp {
+		if cp < rp {
 			return -1
 		}
 		return 1
