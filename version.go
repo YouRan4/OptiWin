4caae88467a5532bca7ee70ca6d0d4ac46839aec
YouRan4 <1670044232@qq.com>
2026-07-22 10:38:03 +0800

    chore: 版本号 v1.3.3

--- version.go
+++ version.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-const CurrentVersion = "v1.4.0"
+const CurrentVersion = "v1.3.3"
 
 func parseVersion(v string) (major, minor, patch int) {
 	v = strings.TrimPrefix(v, "v")
