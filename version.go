373dc7b23adc6cf230a3df550a6bc6ebaf3b8405
YouRan4 <1670044232@qq.com>
2026-08-23 00:21:34 +0800

    v1.7.0: 更新版本号与变更日志

--- version.go
+++ version.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-const CurrentVersion = "v1.6.0"
+const CurrentVersion = "v1.7.0"
 
 func parseVersion(v string) (major, minor, patch int) {
 	v = strings.TrimPrefix(v, "v")
